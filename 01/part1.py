1d1f8f7dcb3e0465edabf8594d0f7f3e37ea97dd
jerrenss <gerrenseow@gmail.com>
2024-12-02 00:36:17 +0800

    solve day 1 part 1

---
+++ 01/part1.py
@@ -0,0 +1,23 @@
+from typing import List
+
+def solution(fileName: str) -> int:
+    leftList: List[int] = []
+    rightList: List[int] = []
+    
+    with open(fileName, 'r') as file:
+        for line in file:
+            numbers = line.split()
+            leftList.append(numbers[0])
+            rightList.append(numbers[1])
+
+    leftList.sort()
+    rightList.sort()
+    
+    N: int = len(leftList)
+    result: int = 0
+    for i in range(N):
+        result += abs(int(leftList[i]) - int(rightList[i]))
+
+    return result
+
+print(solution("./part1-input.txt"))
